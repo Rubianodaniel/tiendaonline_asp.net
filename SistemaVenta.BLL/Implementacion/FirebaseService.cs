4866f3f6e338d3c64eb84bd5b38eb5dfea89f212
Daniel Rubiano <danielrubianomeneses@gmail.com>
2023-03-13 22:42:04 -0500

    este es el primer commit, el proyecto ya esta avanzado ya tiene las vistas generales, se implemento las funcionalidad de enviar correo, y el crud

---
+++ SistemaVenta.BLL/Implementacion/FirebaseService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.Entity;
+using SistemaVenta.DAL;
+
+using Firebase.Auth;
+using Firebase.Storage;
+using SistemaVenta.DAL.Interfaces;
+
+namespace SistemaVenta.BLL.Implementacion
+{
+    public class FirebaseService : IFirebaseService
+    {
+
+        private readonly IGenericRepository<Configuracion> _repository;
+
+        public FirebaseService(IGenericRepository<Configuracion> repository)
+        {
+            _repository = repository;
+        }
+
+
+
+        public Task<string> SubirStorage(Stream StreamArchivo, string CarpetaDestino, string NombreArchivo)
+        {
+            string urlImagen = "";
+
+            try
+            {
+
+            }catch(Exception ex)
+            {
+
+            }
+        }
+        public Task<string> EliminarStorage(string CarpetaDestino, string NombreArchivo)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.DAL.Implementacion;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.BLL.Implementacion;
+
+
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration configuration)
+        {
+            /// conexion a la base de datos 
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+            });
+
+            // inyeccion de dependicias de genericrepository 
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            // inyeccion de dependia de la interface Iventarepository a la casa ventarepository
+            services.AddScoped<IventaRepository, VentaRepository>();
+
+
+            /// inyeccion de dependencia de envio de correo
+            services.AddScoped<ICorreoService, CorreoService>();
+
+
+        }
+    }
+}
